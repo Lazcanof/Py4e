1251e5595415405ea212beac3155769417a469f7
Francisco <68933233+Lazcanof@users.noreply.github.com>
2021-01-28 14:48:31 -0300

    Add files via upload

---
+++ 05_Capstone/honor/gmane/gline.js
@@ -0,0 +1,5 @@
+gline = [ ['Year','umich.edu','cam.ac.uk','indiana.edu','uct.ac.za','gmail.com','berkeley.edu','unicon.net','stanford.edu','ucdavis.edu','mac.com'],
+['2005',57,7,12,14,10,12,6,4,11,12],
+['2006',1319,599,638,428,434,403,390,265,302,404],
+['2007',1113,701,472,463,444,378,351,363,221,90]
+];
